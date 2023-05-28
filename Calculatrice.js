715a8e4e4b28aa00e9204b6237a1b2e66728e411
Merieme-elg <meriemelgoussairi@gmail.com>
2023-05-28 13:42:46 +0100

    modifications on html and css, create functions in Js file to calculate using js

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,1 +1,20 @@
+//------------- variables --------------------//
+screen = document.getElementById("screen");
 
+
+
+//------------- functions --------------------//
+
+
+function clearScreen()
+{
+    screen.value='';
+}
+function display(c)
+{
+    screen.value +=c;
+}
+function calculate()
+{
+    screen.value=eval(screen.value)
+}
