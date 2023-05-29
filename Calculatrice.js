274876cdd1d43427b2a76255a795014ec1ed4065
Merieme-elg <meriemelgoussairi@gmail.com>
2023-05-29 12:09:15 +0100

    ajout des options : ignorer les 2 operations, ignorer le comancement par une operation

--- Calculatrice.js
+++ Calculatrice.js
@@ -1,8 +1,8 @@
 //------------- variables --------------------//
-screen = document.getElementById("screen");
-
-
-
+var screen = document.getElementById("screen");
+var currentValue, preVal; //les valeurs actuelle et l'avant actuelle//
+var taille; //taille des valeurs insérer// 
+var operations = ["+","-","*","/"];
 //------------- functions --------------------//
 
 
@@ -10,11 +10,45 @@ function clearScreen()
 {
     screen.value='';
 }
+
 function display(c)
 {
     screen.value +=c;
+    currentValue = c;
+    taille = screen.value.length;
+    prevCurVal();
+    compare();
 }
+
 function calculate()
 {
-    screen.value=eval(screen.value)
+    screen.value=eval(screen.value);
+}
+
+function prevCurVal()
+{
+    preVal=screen.value.substring(taille-2,taille-1);
+}
+
+function compare()
+{   if(operations.includes(currentValue)&&taille==1){removeNext()}
+    if((operations.includes(preVal)) &&
+    (operations.includes(currentValue))){
+        if(preVal==currentValue)
+            {removeNext()}
+        else
+            {removeBouth()}
+    } 
+}
+
+function removeNext()
+{
+    screen.value=screen.value.substring(0,taille-1);
+
+}
+
+function removeBouth()
+{
+    screen.value=screen.value.substring(0,taille-2)+screen.value.substring(taille-1,taille);
+
 }
